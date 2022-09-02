9d628e49bf5ed89285fd30750f19f0743b7fcff7
Chris Vandborg <chrisvandborg@MacBook-Pro.local>
2022-09-02 23:16:56 +0200

    Add third completed challenge

--- codewars.js
+++ codewars.js
@@ -1,4 +1,4 @@
-//Find Mean
+//#1 Find Mean
 
 
 //Find the mean (average) of a list of numbers in an array.
@@ -8,11 +8,20 @@
 const findAverage =  nums => nums.reduce( (acc, c) =>acc+c) / nums.length
 
 
-//8kyu SURFACE AREA AND VOLUME OF A BOX
+//#2 8kyu SURFACE AREA AND VOLUME OF A BOX
 
 //Write a function that returns the total surface area and volume of a box as an array: [area, volume]
 
 //solution
 function getSize(width, height, depth){
     return [(depth*height*2)+(width*depth*2)+(width*height*2), width*height*depth]
+  }
+
+//#3 Enumerable Magic - Does My List Include This?
+
+//Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+function include(arr, item){
+    // ...
+    return arr.includes(item) ? true : false
   }
