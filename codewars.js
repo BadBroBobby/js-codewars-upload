685545972a395247e3b8802227b6835204e2c315
Chris Vandborg <chrisvandborg@MacBook-Pro.local>
2022-09-02 23:28:17 +0200

    Added another solution

--- codewars.js
+++ codewars.js
@@ -21,7 +21,27 @@ function getSize(width, height, depth){
 
 //Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
 
-function include(arr, item){
+/* function include(arr, item){
     // ...
     return arr.includes(item) ? true : false
+  }
+
+  Messi goals function
+  Messi is a soccer player with goals in three leagues:
+  
+  LaLiga
+  Copa del Rey
+  Champions
+  Complete the function to return his total number of goals in all three leagues.
+  
+  Note: the input will always be valid.
+  
+  For example:
+  
+  5, 10, 2  -->  17 */
+
+//Solution
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    // code goes here
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
   }
