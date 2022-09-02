1ce2a207d28b42638e07d47d3dbd9d0ea8df36a8
Chris Vandborg <chrisvandborg@MacBook-Pro.local>
2022-09-02 22:46:13 +0200

    Added second code commit to GitHub

--- codewars.js
+++ codewars.js
@@ -6,3 +6,13 @@
 //Solution
 
 const findAverage =  nums => nums.reduce( (acc, c) =>acc+c) / nums.length
+
+
+//8kyu SURFACE AREA AND VOLUME OF A BOX
+
+//Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
+//solution
+function getSize(width, height, depth){
+    return [(depth*height*2)+(width*depth*2)+(width*height*2), width*height*depth]
+  }
