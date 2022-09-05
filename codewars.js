5376bb899826fa0bcde0299a88f589c77992793a
Chris Vandborg <chrisvandborg@MacBook-Pro.local>
2022-09-05 22:53:25 +0200

    Anotha one

--- codewars.js
+++ codewars.js
@@ -45,15 +45,26 @@ function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
     // code goes here
     return laLigaGoals + copaDelReyGoals + championsLeagueGoals
   }
-  
- /*  Given a sequence of numbers, find the largest pair sum in the sequence.
 
-  For example */
+ /*  Given a sequence of numbers, find the largest pair sum in the sequence.*/
+
+  //Solution
 
 function largestPairSum (numbers) {
-    //TODO: Write your Code here
     numbers.sort( (a,b) => a-b);
     let firstNum = numbers[numbers.length-1]
     let secondNum = numbers[numbers.length-2]
     return firstNum + secondNum
   }
+
+/* What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
+Example(Input --> Output)
+  
+"apple ban" --> ["apple 5", "ban 3"]
+"you will win" -->["you 3", "will 4", "win 3"]
+Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element .
+  
+Note: String will have at least one element; words will always be separated by a space. */
+
+//Solution
+const addLength = str => str.split(' ').map( word => word + " " + word.length)
